6b426c6bd5e80bbcf0bb325c109f6e7783f8ec76
Moiezequiel <00038221@uca.edu.sv>
2024-06-29 21:08:14 -0600

    add:anonymous visit connection

--- SecurifyClient/src/pages/vigilant/anonymousVisit.jsx
+++ SecurifyClient/src/pages/vigilant/anonymousVisit.jsx
@@ -1,13 +1,19 @@
 import React, { useContext, useState } from 'react';
+import Modal from 'react-modal';
+import axios from 'axios';
 import SidebarVigilant from '../../components/sidebarVigilant';
 import { Link, useNavigate } from 'react-router-dom';
 import { DataContext } from "../../context/DataContext";
 import './vigilant.css';
 
+Modal.setAppElement('#root'); // Asegúrate de que el root coincide con el id del div principal en tu index.html
+
 const AnonymousVisit = () => {
   const { setAnonymousData } = useContext(DataContext);
   const [formValues, setFormValues] = useState({ name: '', descripcion: '' });
-  const [errorMessage, setErrorMessage] = useState('');
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+  const [isSuccess, setIsSuccess] = useState(true);
   const navigate = useNavigate();
 
   const handleInputChange = (e) => {
@@ -18,17 +24,37 @@ const AnonymousVisit = () => {
     });
   };
 
-  const handleGenerate = () => {
+  const handleGenerate = async () => {
     if (!formValues.name || !formValues.descripcion) {
-      setErrorMessage('Por favor, complete todos los campos.');
+      setIsSuccess(false);
+      setModalMessage('Por favor, complete todos los campos.');
+      setModalIsOpen(true);
       return;
     }
-    setAnonymousData((prevData) => [...prevData, formValues]);
-    navigate('/AnonymousHistory');
+
+    try {
+      const data = {
+        name: formValues.name,
+        description: formValues.descripcion
+      };
+      const response = await axios.post('http://localhost:8080/api/visits/anonymous', data, {
+        headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+      });
+      setIsSuccess(true);
+      setModalMessage(response.data.message || 'La solicitud ha sido generada con éxito.');
+      setModalIsOpen(true);
+      setAnonymousData((prevData) => [...prevData, formValues]);
+      navigate('/AnonymousHistory');
+    } catch (error) {
+      setIsSuccess(false);
+      setModalMessage(error.response ? error.response.data.message : 'Ocurrió un error al añadir la entrada');
+      setModalIsOpen(true);
+      console.log(error);
+    }
   };
 
-  const handleCloseError = () => {
-    setErrorMessage('');
+  const closeModal = () => {
+    setModalIsOpen(false);
   };
 
   return (
@@ -69,19 +95,30 @@ const AnonymousVisit = () => {
             </Link>
           </div>
         </div>
-        {errorMessage && (
-          <div className="error-popup">
-            <div className="error-popup-content">
-              <p>{errorMessage}</p>
-              <button onClick={handleCloseError}>Cerrar</button>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Mensaje del formulario"
+          className="custom-modal"
+          overlayClassName="custom-overlay"
+        >
+          <div className="modal-content">
+            <div className={`modal-icon ${isSuccess ? 'success' : 'error'}`}>
+              <span>{isSuccess ? '✓' : 'X'}</span>
             </div>
+            <h2 className="modal-title">{isSuccess ? 'Éxito' : '¡Error!'}</h2>
+            <p className="modal-message">{modalMessage}</p>
+            <button 
+              onClick={closeModal} 
+              className={`modal-button ${isSuccess ? '' : 'error'}`}
+            >
+              Continuar
+            </button>
           </div>
-        )}
+        </Modal>
       </div>
     </>
   );
 };
 
 export default AnonymousVisit;
-
-
