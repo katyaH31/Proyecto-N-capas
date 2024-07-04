b14a73bb85cd87f74acac7b6437005f89eece783
Moiezequiel <00038221@uca.edu.sv>
2024-07-04 11:59:51 -0600

    add:base url in userPanel

--- SecurifyClient/src/pages/Administrator/userPanel.jsx
+++ SecurifyClient/src/pages/Administrator/userPanel.jsx
@@ -1,18 +1,47 @@
 import React, { useState, useRef, useEffect } from 'react';
+import Modal from 'react-modal';
+import axios from 'axios';
+import { baseURL } from '../../config/apiConfig'; // Importar baseURL
 import './admi.css';
+
+Modal.setAppElement('#root'); // Asegúrate de que el root coincide con el id del div principal en tu index.html
+
 const UserPanel = () => {
   const [filterText, setFilterText] = useState('');
   const [newData, setNewData] = useState([]);
+  const [backendData, setBackendData] = useState([]);
   const [formValues, setFormValues] = useState({
-    name: '',
-    dui: '',
+    username: '',
     casa: '',
     correo: '',
-    representante: '',
     acciones: 'Acciones', // Default value for actions
   });
-
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+  const [isSuccess, setIsSuccess] = useState(true);
   const tableContainerRef = useRef(null);
+
+  useEffect(() => {
+    // Fetch data from the backend
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(baseURL + 'user/allna', {
+          headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+        });
+        console.log('Backend response:', response);
+        // Añadir campo 'acciones' a cada elemento del backend
+        const dataWithActions = response.data.data.map((item) => ({
+          ...item,
+          acciones: 'Acciones'
+        }));
+        setBackendData(dataWithActions);
+      } catch (error) {
+        console.error('Error fetching data:', error.response ? error.response.data : error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleFilterChange = (e) => {
     setFilterText(e.target.value);
@@ -26,20 +55,56 @@ const UserPanel = () => {
     });
   };
 
-  const handleAddData = () => {
-    const newEntry = {
-      ...formValues,
-      casa: parseInt(formValues.casa, 10), // Convert casa to number
-    };
-    setNewData((prevData) => [...prevData, newEntry]);
-    setFormValues({
-      name: '',
-      dui: '',
-      casa: '',
-      correo: '',
-      representante: '',
-      acciones: 'Acciones',
-    });
+  const handleAddData = async () => {
+    console.log('Nombre de usuario:', formValues.username);
+    console.log('Casa:', formValues.casa);
+    console.log('Correo:', formValues.correo);
+
+    if (!formValues.username || !formValues.casa || !formValues.correo) {
+      setIsSuccess(false);
+      setModalMessage('Por favor, complete todos los campos.');
+      setModalIsOpen(true);
+      return;
+    }
+
+    const casaValue = parseInt(formValues.casa, 10);
+    if (isNaN(casaValue)) {
+      setIsSuccess(false);
+      setModalMessage('Número de casa debe ser un número.');
+      setModalIsOpen(true);
+      return;
+    }
+
+    try {
+      const data = {
+        username: formValues.username,
+        houseId: casaValue,
+        email: formValues.correo,
+        acciones: 'Acciones',
+      };
+
+      const response = await axios.post(baseURL + 'users', data, {
+        headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+      });
+
+      console.log('Response:', response.data);
+
+      setIsSuccess(true);
+      setModalMessage(response.data.message || 'La entrada se ha agregado con éxito.');
+      setModalIsOpen(true);
+      setNewData((prevData) => [...prevData, data]);
+      setFormValues({
+        username: '',
+        casa: '',
+        correo: '',
+        acciones: 'Acciones',
+      });
+    } catch (error) {
+      console.error('Error:', error.response ? error.response.data : error.message);
+      setIsSuccess(false);
+      setModalMessage(error.response ? error.response.data.message : 'Ocurrió un error al agregar la entrada');
+      setModalIsOpen(true);
+    }
   };
 
   useEffect(() => {
@@ -48,30 +113,24 @@ const UserPanel = () => {
     }
   }, [newData]);
 
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   const columns = [
     {
-      name: 'Nombre',
-      selector: (row) => row.name,
-      sortable: true,
-    },
-    {
-      name: 'Número de DUI',
-      selector: (row) => row.dui,
+      name: 'Nombre de usuario',
+      selector: (row) => row.username,
       sortable: true,
     },
     {
       name: 'Casa',
-      selector: (row) => row.casa,
+      selector: (row) => row.houseId || row.casa,
       sortable: true,
     },
     {
       name: 'Correo',
-      selector: (row) => row.correo,
-      sortable: true,
-    },
-    {
-      name: 'Representante',
-      selector: (row) => row.representante,
+      selector: (row) => row.email || row.correo,
       sortable: true,
     },
     {
@@ -81,43 +140,7 @@ const UserPanel = () => {
     },
   ];
 
-  const data = [
-    {
-      name: 'Katya',
-      dui: '060987623-1',
-      casa: 13,
-      correo: 'lisbetherrera98@gmail.com',
-      representante: 'SI SOY',
-      acciones: 'Acciones',
-    },
-    {
-      name: 'David',
-      dui: '060987623-1',
-      casa: 13,
-      correo: 'lisbeloku98@gmail.com',
-      representante: 'SI SOY',
-      acciones: 'Acciones',
-    },
-    {
-      name: 'Cardo',
-      dui: '060987623-1',
-      casa: 13,
-      correo: 'lisbeloku98@gmail.com',
-      representante: 'HOLI',
-      acciones: 'Acciones',
-    },
-    {
-      name: 'Moi',
-      dui: '060987623-1',
-      casa: 13,
-      correo: 'lisbeloku98@gmail.com',
-      representante: 'HOLI',
-      acciones: 'Acciones',
-    },
-    ...newData, // Agregar nuevos datos
-  ];
-
-  const filteredData = data.filter((item) =>
+  const filteredData = [...backendData, ...newData].filter((item) =>
     Object.values(item).some((value) =>
       value.toString().toLowerCase().includes(filterText.toLowerCase())
     )
@@ -139,18 +162,10 @@ const UserPanel = () => {
         <div className="form">
           <input
             type="text"
-            name="name"
+            name="username"
             className="form-input"
-            placeholder="Nombre"
-            value={formValues.name}
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="dui"
-            className="form-input"
-            placeholder="N° de DUI"
-            value={formValues.dui}
+            placeholder="Nombre de usuario"
+            value={formValues.username}
             onChange={handleInputChange}
           />
           <input
@@ -169,36 +184,24 @@ const UserPanel = () => {
             value={formValues.correo}
             onChange={handleInputChange}
           />
-          <input
-            type="text"
-            name="representante"
-            className="form-input"
-            placeholder="Representante"
-            value={formValues.representante}
-            onChange={handleInputChange}
-          />
           <button className="form-button" onClick={handleAddData}>Agregar a la tabla</button>
         </div>
         <div className="custom-table-wrapper text-sm" ref={tableContainerRef}>
           <table className="custom-table">
             <thead>
               <tr>
-                <th>Nombre</th>
-                <th>N° de DUI</th>
+                <th>Nombre de usuario</th>
                 <th>Casa</th>
                 <th>Correo</th>
-                <th>Represenntante</th>
                 <th>Acciones</th>
               </tr>
             </thead>
             <tbody>
               {filteredData.map((row, index) => (
                 <tr key={index}>
-                  <td data-label="Nombre">{row.name}</td>
-                  <td data-label="N° de DUI">{row.dui}</td>
-                  <td data-label="Casa">{row.casa}</td>
-                  <td data-label="Correo">{row.casa}</td>
-                  <td data-label="Representante">{row.representante}</td>
+                  <td data-label="Nombre de usuario">{row.username}</td>
+                  <td data-label="Casa">{row.houseId || row.casa}</td>
+                  <td data-label="Correo">{row.email || row.correo}</td>
                   <td data-label="Acciones">{row.acciones}</td>
                 </tr>
               ))}
@@ -206,6 +209,27 @@ const UserPanel = () => {
           </table>
         </div>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Mensaje del formulario"
+        className="custom-modal"
+        overlayClassName="custom-overlay"
+      >
+        <div className="modal-content">
+          <div className={`modal-icon ${isSuccess ? 'success' : 'error'}`}>
+            <span>{isSuccess ? '✓' : 'X'}</span>
+          </div>
+          <h2 className="modal-title">{isSuccess ? 'Éxito' : '¡Error!'}</h2>
+          <p className="modal-message">{modalMessage}</p>
+          <button 
+            onClick={closeModal} 
+            className={`modal-button ${isSuccess ? '' : 'error'}`}
+          >
+            Continuar
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
