cd8595f12923a1d3427989b9f0161744900ff36c
davidperlera2 <00165220@uca.edu.sv>
2024-07-06 21:52:49 -0600

    add: resident and resident inCharge Qr generator

--- SecurifyClient/src/pages/residentAdmin/qrgeneratora.jsx
+++ SecurifyClient/src/pages/residentAdmin/qrgeneratora.jsx
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import QRCode from 'qrcode.react';
-import './residenta.css'; // 
+import axios from 'axios';
+import { baseURL } from '../../config/apiConfig'; // Importar baseURL
+import './residenta.css';
 
 const QRGeneratora = () => {
   const [qrValue, setQrValue] = useState(''); // Inicialmente vacío
   const [buttonDisabled, setButtonDisabled] = useState(false); // Controla la deshabilitación del botón
   const [timeLeft, setTimeLeft] = useState(0); // Estado para el contador
 
-  const generateQR = () => {
-    setQrValue('https://new-url.cosaasasasm');
-    setButtonDisabled(true); // Deshabilita el botón cuando se genera el código QR
-    setTimeLeft(120); // Inicializa el contador a 120 segundos (2 minutos)
+  const generateQR = async () => {
+    try {
+      const response = await axios.post(baseURL + 'qr/create', {}, {
+        headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+      });
+      const token = response.data.data; // El token recibido
+      setQrValue(token);
+      setButtonDisabled(true); // Deshabilita el botón cuando se genera el código QR
+      setTimeLeft(120); // Inicializa el contador a 120 segundos (2 minutos)
+    } catch (error) {
+      console.error('Error generating QR:', error.response ? error.response.data : error.message);
+    }
   };
 
   useEffect(() => {
@@ -26,36 +36,36 @@ const QRGeneratora = () => {
   }, [buttonDisabled, timeLeft]);
 
   return (
-    <div className="qr-generator-container">
-       <aside className="sidebar" style={{ backgroundColor: 'white' }}>
-        {/* Contenido del aside */}
-      </aside>
-      <div className="main-container">
-        <div className="content-container">
-          <div className="left-content">
-            {/* Contenido izquierdo */}
-          </div>
-          <div className="right-content">
-            {/* Renderiza el código QR solo si qrValue no está vacío */}
-            {qrValue && (
-              <div className="qr-code">
-                <QRCode value={qrValue} size={256} />
-              </div>
-            )}
-            <div className="button-container">
-              <button onClick={generateQR} className="generate-button" disabled={buttonDisabled}>
-                Generar QR
-              </button>
-              {buttonDisabled && (
-                <div className="timer">
-                  Tiempo restante para generar un nuevo código: {`${Math.floor(timeLeft / 60)}:${('0' + (timeLeft % 60)).slice(-2)}`}
-                </div>
+      <div className="qr-generator-container">
+        <aside className="sidebar" style={{ backgroundColor: 'white' }}>
+          {/* Contenido del aside */}
+        </aside>
+        <div className="main-container">
+          <div className="content-container">
+            <div className="left-content">
+              {/* Contenido izquierdo */}
+            </div>
+            <div className="right-content">
+              {/* Renderiza el código QR solo si qrValue no está vacío */}
+              {qrValue && (
+                  <div className="qr-code">
+                    <QRCode value={qrValue} size={256} />
+                  </div>
               )}
+              <div className="button-container">
+                <button onClick={generateQR} className="generate-button" disabled={buttonDisabled}>
+                  Generar QR
+                </button>
+                {buttonDisabled && (
+                    <div className="timer">
+                      Tiempo restante para generar un nuevo código: {`${Math.floor(timeLeft / 60)}:${('0' + (timeLeft % 60)).slice(-2)}`}
+                    </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
