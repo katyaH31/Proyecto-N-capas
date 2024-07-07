1a08e5e5849829214adab4ef59eb89ca682edf7f
davidperlera2 <00165220@uca.edu.sv>
2024-07-07 11:56:09 -0600

    add: add resident to a house by manager

--- SecurifyClient/src/pages/residentAdmin/homeresidents.jsx
+++ SecurifyClient/src/pages/residentAdmin/homeresidents.jsx
@@ -8,10 +8,7 @@ Modal.setAppElement('#root'); // Asegúrate de que el root coincide con el id de
 
 const HomeResidents = ({ houseId }) => {
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    duiNumber: '',
-    email: ''
+    username: '',
   });
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [modalMessage, setModalMessage] = useState('');
@@ -45,7 +42,7 @@ const HomeResidents = ({ houseId }) => {
     e.preventDefault();
     if (!validateForm()) {
       setIsSuccess(false); // Error en la validación
-      setModalMessage('¡Lo sentimos! La solicitud no se pudo realizar.');
+      setModalMessage('Por favor, ingrese el nombre de usuario.');
       setModalIsOpen(true);
       return;
     }
@@ -55,15 +52,12 @@ const HomeResidents = ({ houseId }) => {
         headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
       });
       setIsSuccess(true); // Éxito en la validación
-      setModalMessage('El residente ha sido incorporado.');
+      setModalMessage(response.data.message || 'El residente ha sido incorporado.');
       setModalIsOpen(true);
       setResidents((prevResidents) => [...prevResidents, response.data]); // Actualiza la lista de residentes
       // Reset form data
       setFormData({
-        firstName: '',
-        lastName: '',
-        duiNumber: '',
-        email: ''
+        username: '',
       });
     } catch (error) {
       const errorMessage = error.response ? error.response.data.message : 'Ocurrió un error al añadir la entrada';
@@ -75,13 +69,7 @@ const HomeResidents = ({ houseId }) => {
   };
 
   const validateForm = () => {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return (
-      formData.firstName.trim() !== '' &&
-      formData.lastName.trim() !== '' &&
-      /^\d{9}$/.test(formData.duiNumber.replace(/-/g, '')) &&
-      emailPattern.test(formData.email)
-    );
+    return formData.username.trim() !== '';
   };
 
   const closeModal = () => {
@@ -89,100 +77,64 @@ const HomeResidents = ({ houseId }) => {
   };
 
   return (
-    <div className="home-residents">
-      <aside className="sidebar">
-        {/* Contenido del aside */}
-      </aside>
-      <div className="main-container">
-        <form onSubmit={handleSubmit} className="space-y-4 p-6 max-w-lg mx-auto content-container">
-          <div className="left-content">
-            <h1 className="text-2xl font-bold mb-4">Rellenar los campos para registrar Residente</h1>
-            <div>
-              <label htmlFor="firstName" className="block text-gray-700 text-left">Nombre:</label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                value={formData.firstName}
-                onChange={handleChange}
-                className="mt-1 p-2 w-full border border-gray-300 rounded-md"
-              />
+      <div className="home-residents">
+        <aside className="sidebar">
+          {/* Contenido del aside */}
+        </aside>
+        <div className="main-container">
+          <form onSubmit={handleSubmit} className="space-y-4 p-6 max-w-lg mx-auto content-container">
+            <div className="left-content">
+              <h1 className="text-2xl font-bold mb-4">Registrar Residente</h1>
+              <div>
+                <label htmlFor="username" className="block text-gray-700 text-left">Usuario o Correo:</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                    className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+                    placeholder="Username or Email"
+                />
+              </div>
+              <div className="button-container">
+                <button type="submit" className="generate-button text-left">Registrar Residente</button>
+              </div>
             </div>
-            <div>
-              <label htmlFor="lastName" className="block text-gray-700 text-left">Apellido:</label>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                value={formData.lastName}
-                onChange={handleChange}
-                className="mt-1 p-2 w-full border border-gray-300 rounded-md"
-              />
-            </div>
-            <div>
-              <label htmlFor="duiNumber" className="block text-gray-700 text-left">Número de DUI:</label>
-              <input
-                type="text"
-                id="duiNumber"
-                name="duiNumber"
-                value={formData.duiNumber}
-                onChange={handleChange}
-                className="mt-1 p-2 w-full border border-gray-300 rounded-md"
-                maxLength="10"
-                pattern="\d{8}-\d"
-                title="El número de DUI debe contener 9 dígitos en el formato 00000000-0"
-                placeholder="00000000-0"
-              />
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-gray-700 text-left">Correo electrónico:</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="mt-1 p-2 w-full border border-gray-300 rounded-md"
-              />
-            </div>
-            <div className="button-container">
-              <button type="submit" className="generate-button text-left">Registrar Residente</button>
-            </div>
+          </form>
+          <div className="residents-list">
+            <h2 className="text-xl font-bold mb-4 text-center">Residentes actuales</h2>
+            <ul>
+              {residents.map((resident) => (
+                  <li key={resident.id} className="resident-item">
+                    {resident.username}
+                  </li>
+              ))}
+            </ul>
           </div>
-        </form>
-        <div className="residents-list">
-          <h2 className="text-xl font-bold mb-4 text-center">Residentes actuales</h2>
-          <ul>
-            {residents.map((resident) => (
-              <li key={resident.id} className="resident-item">
-                {resident.firstName} {resident.lastName} - {resident.duiNumber} - {resident.email}
-              </li>
-            ))}
-          </ul>
         </div>
+        <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Mensaje del formulario"
+            className="custom-modal"
+            overlayClassName="custom-overlay"
+        >
+          <div className="modal-content">
+            <div className={`modal-icon ${isSuccess ? 'success' : 'error'}`}>
+              <span>{isSuccess ? '✓' : 'X'}</span>
+            </div>
+            <h2 className="modal-title">{isSuccess ? 'Bienvenido' : '¡Lo sentimos!'}</h2>
+            <p className="modal-message">{modalMessage}</p>
+            <button
+                onClick={closeModal}
+                className={`modal-button ${isSuccess ? '' : 'error'}`}
+            >
+              Continuar
+            </button>
+          </div>
+        </Modal>
       </div>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Mensaje del formulario"
-        className="custom-modal"
-        overlayClassName="custom-overlay"
-      >
-        <div className="modal-content">
-          <div className={`modal-icon ${isSuccess ? 'success' : 'error'}`}>
-            <span>{isSuccess ? '✓' : 'X'}</span>
-          </div>
-          <h2 className="modal-title">{isSuccess ? 'Bienvenido' : '¡Lo sentimos!'}</h2>
-          <p className="modal-message">{modalMessage}</p>
-          <button 
-            onClick={closeModal} 
-            className={`modal-button ${isSuccess ? '' : 'error'}`}
-          >
-            Continuar
-          </button>
-        </div>
-      </Modal>
-    </div>
   );
 };
 
